20d7a035236ffbade884ba2ac61cedda9ec4d751
Prabashree17 <166893334+Prabashree17@users.noreply.github.com>
2025-07-19 12:16:33 +0530

    Add files via upload

---
+++ Evennumber.java
@@ -0,0 +1,18 @@
+package Flowcontrol;
+
+public class Evennumber {
+
+	public static void main(String[] args) {
+		
+		        for (int i = 23; i <= 57; i++) {
+		            if (i % 2 == 0) {
+		                System.out.println(i);
+		            }
+		        }
+		    }
+		}
+
+
+	
+
+
